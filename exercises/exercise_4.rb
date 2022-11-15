725e0f0b439677a2bd886939d76b4f4c670e94ee
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-15 12:50:15 -0800

    Assign men's/women's apparel to their own instance variable, print all men's stores and print women's stores making less than M rev

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,26 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+# Assign men's apparel to instance variable and print each one
+# Assign women's apparel to instance variable and print ones making less than $1M rev
+
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_store = Store.where(mens_apparel: true)
+
+@mens_store.each do |store| 
+  puts "Men's Store: #{store.name}, annual revenue: #{store.annual_revenue}"
+end
+
+@womens_store = Store.where(womens_apparel: true)
+
+@womens_store.each do |store|
+  if store.annual_revenue < 1000000
+    puts "Women's Store: #{store.name}"
+  end
+end

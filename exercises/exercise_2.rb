0e237299516d6f178869fe585dda4475ea79027d
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-15 12:26:10 -0800

    Load two stores, assign instance variable to them and rename one of them

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,15 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
+# Load two particular stores and assign to an instance variable
+# Rename store1 to different name
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: '1')
+
+@store2 = Store.find_by(id: '2')
+
+@store1.name = "Burnaby - Metro area"
+
+@store1.save

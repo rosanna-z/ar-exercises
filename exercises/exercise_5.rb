a2ac4056272230ff733c9fb3f8022be3e989354b
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-15 13:10:08 -0800

    Calculate total annual rev for all stores, avg rev for all stores and number of stores generating over M in rev

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,18 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+# Total annual rev for all stores
+# Avg rev for all stores
+# Number of stores generating $1M+ rev
+
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+
+@high_sales_stores = Store.where('annual_revenue < ?', 1000000)
+
+puts @high_sales_stores.count
+
